42409f8e05fa6d9d409287f69616ca6df31e6fbe
yamadapc <tacla.yamada@gmail.com>
2017-07-13 08:34:00 -0300

    Reformat and lint

--- src/StackBump/Main.hs
+++ src/StackBump/Main.hs
@@ -4,146 +4,150 @@ module StackBump.Main where
 
 import           Prelude               hiding (readFile)
 
-import           Control.Concurrent
-import           Control.Concurrent.Async
-import           Control.Exception
 import           Control.Lens          hiding ((.=))
 import           Control.Monad
 import           Data.Aeson.Lens
 import qualified Data.ByteString.Char8 as ByteString (pack, unpack)
 import           Data.List
 import           Data.Maybe
-import           Data.Text             (Text)
+import           Data.Monoid
 import qualified Data.Text             as Text
 import           Data.Yaml
-import           Options.Applicative
 import           System.Console.ANSI
 import           System.Environment
 import           System.Exit
-import           System.IO (hPutStrLn, stderr)
-import           System.IO.Strict
 import           System.FilePath
 import           System.FilePath.Glob
-import           System.Process
+import           System.IO.Strict
 import           Text.Read
 
 import           StackBump.Spinner
 
-data BumpType = BumpTypeOther Int
-              | BumpTypePatch
-              | BumpTypeMinor
-              | BumpTypeMajor
-    deriving(Show, Eq)
+data BumpType
+  = BumpTypeOther Int
+  | BumpTypePatch
+  | BumpTypeMinor
+  | BumpTypeMajor
+  deriving (Show, Eq)
 
-data Package = Package String
+data Package =
+  Package String
 
 bumpPackage :: BumpType -> IO (Either String (String, String))
 bumpPackage bt = do
-    !pkg <- lines <$> readFile "package.yaml"
-    let i = findIndex ("version" `isPrefixOf`) pkg
-    case i of
-        Nothing -> return (Left "No `version` to bump")
-        Just i -> do
-            let (p, (versionStr:ps)) = splitAt i pkg -- Partial, but can't be
-                ev = decodeEither (ByteString.pack versionStr) :: Either String Value
-                vstring = ev ^. _Right . key "version" . _String
-                vstringS = map Text.unpack (Text.split (== '.') vstring)
-                ebv = intercalate "." <$> bump bt vstringS
-            case ebv of
-                Left e -> return (Left e)
-                Right bv -> do
-                    let packageYaml' = unlines ( p
-                                              <> [ init (ByteString.unpack (encode (object ["version" .= bv])))
-                                                 ]
-                                              <> ps
-                                               )
-                    return (Right (packageYaml', bv))
+  !pkg <- lines <$> readFile "package.yaml"
+  let mi = findIndex ("version" `isPrefixOf`) pkg
+  case mi of
+    Nothing -> return (Left "No `version` to bump")
+    Just i -> do
+      let (p, versionStr:ps) = splitAt i pkg -- Partial, but can't be
+          ev = decodeEither (ByteString.pack versionStr) :: Either String Value
+          vstring = ev ^. _Right . key "version" . _String
+          vstringS = map Text.unpack (Text.split (== '.') vstring)
+          ebv = intercalate "." <$> bump bt vstringS
+      case ebv of
+        Left e -> return (Left e)
+        Right bv -> do
+          let packageYaml' =
+                unlines
+                  (p <>
+                   [ init
+                       (ByteString.unpack (encode (object ["version" .= bv])))
+                   ] <>
+                   ps)
+          return (Right (packageYaml', bv))
 
 bump :: BumpType -> [String] -> Either String [String]
-bump BumpTypeMajor (n:ns) = (:(map (const "0") ns)) . show . (+1) <$> readEither n
+bump BumpTypeMajor (n:ns) =
+  (: map (const "0") ns) . show . (+ (1 :: Int)) <$> readEither n
 bump BumpTypeMajor _ = Left "Can't major bump"
-bump BumpTypeMinor (n1:n:ns) = (\x -> n1:x:(map (const "0") ns)) . show . (+1) <$> readEither n
+bump BumpTypeMinor (n1:n:ns) = (\x -> n1:x:map (const "0") ns) . show . (+(1 :: Int)) <$> readEither n
 bump BumpTypeMinor _ = Left "Can't minor bump"
-bump BumpTypePatch (n1:n2:n:ns) = (\x -> n1:n2:x:(map (const "0") ns)) . show . (+1) <$> readEither n
+bump BumpTypePatch (n1:n2:n:ns) =
+  (\x -> n1 : n2 : x : map (const "0") ns) . show . (+ (1 :: Int)) <$>
+  readEither n
 bump BumpTypePatch _ = Left "Can't patch bump"
-bump (BumpTypeOther c) ns = if c >= length ns
+bump (BumpTypeOther c) ns =
+  if c >= length ns
     then Left ("Can't bump " <> show c <> " component")
-    else let (n1, (n:n2)) = splitAt c ns
-         in (\x -> n1 <> (x:map (const "0") n2)) . show . (+1) <$> readEither n
+    else let (n1, n:n2) = splitAt c ns
+         in (\x -> n1 <> (x : map (const "0") n2)) . show . (+ (1 :: Int)) <$>
+            readEither n
 
 readBumpType :: [String] -> Either String BumpType
-readBumpType as = case as of
-    ("other":[]) -> Left "Usage: stack-bump other <n>"
+readBumpType as =
+  case as of
+    ["other"]     -> Left "Usage: stack-bump other <n>"
     ("other":x:_) -> BumpTypeOther <$> readEither x
-    ("patch":_) -> Right BumpTypePatch
-    ("minor":_) -> Right BumpTypeMinor
-    ("major":_) -> Right BumpTypeMajor
-    _ -> Left "Usage: stack-bump <patch|minor|major|other <n>>"
+    ("patch":_)   -> Right BumpTypePatch
+    ("minor":_)   -> Right BumpTypeMinor
+    ("major":_)   -> Right BumpTypeMajor
+    _             -> Left "Usage: stack-bump <patch|minor|major|other <n>>"
 
+runTasks :: String -> IO a -> IO ()
 runTasks title action = do
-    setSGR [SetColor Foreground Vivid Yellow]
-    putChar '•'
-    setSGR [SetColor Foreground Vivid Black]
-    putStrLn (" " <> title)
-    action
-    cursorUp 1
-    clearLine
-    setCursorColumn 0
-    setSGR [SetColor Foreground Vivid Green]
-    putChar '✓'
-    setSGR [SetColor Foreground Vivid Black]
-    putStrLn (" " <> title)
+  setSGR [SetColor Foreground Vivid Yellow]
+  putChar '•'
+  setSGR [SetColor Foreground Vivid Black]
+  putStrLn (" " <> title)
+  _ <- action
+  cursorUp 1
+  clearLine
+  setCursorColumn 0
+  setSGR [SetColor Foreground Vivid Green]
+  putChar '✓'
+  setSGR [SetColor Foreground Vivid Black]
+  putStrLn (" " <> title)
 
 run :: BumpType -> IO ()
 run bt = do
-    ev <- bumpPackage bt
-    case ev of
-        Left e -> error e
-        Right (packageYaml', v) -> do
-            runTasks "Checking if package is good for publishing" $ do
-                runProcessWithSpinner "stack build"
-                runProcessWithSpinner "stack test"
-                runProcessWithSpinner "stack sdist"
+  ev <- bumpPackage bt
+  case ev of
+    Left e -> error e
+    Right (packageYaml', v) -> do
+      runTasks "Checking if package is good for publishing" $ do
+        runProcessWithSpinner "stack build"
+        runProcessWithSpinner "stack test"
+        runProcessWithSpinner "stack sdist"
+      runTasks ("Writting new version (v" <> v <> ")") $
+        writeFile "package.yaml" packageYaml'
+      runTasks ("Commiting (v" <> v <> ")") $ do
+        runProcessWithSpinner "stack build"
+        runProcessWithSpinner "git add package.yaml"
+        mcabalFile <- findCabalfile
+        case mcabalFile of
+          Just cabalFile -> runProcessWithSpinner ("git add " <> cabalFile)
+          Nothing        -> return ()
+        runProcessWithSpinnerRaw
+          "git commit -m ..."
+          "git"
+          ["commit", "-m", "Bump version to v" <> v]
+        runProcessWithSpinner ("git tag v" <> v)
+      putStrLn ""
+      setSGR [SetColor Foreground Vivid White]
+      putStrLn ("Bumped version to: v" <> v)
 
-            runTasks ("Writting new version (v" <> v <> ")") $ do
-                writeFile "package.yaml" packageYaml'
-
-            runTasks ("Commiting (v" <> v <> ")") $ do
-                runProcessWithSpinner ("stack build")
-                runProcessWithSpinner "git add package.yaml"
-                mcabalFile <- findCabalfile
-                case mcabalFile of
-                    Just cabalFile -> runProcessWithSpinner ("git add " <> cabalFile)
-                    Nothing -> return ()
-                runProcessWithSpinnerRaw "git commit -m ..." "git" ["commit", "-m", "Bump version to v" <> v]
-                runProcessWithSpinner ("git tag v" <> v)
-
-            putStrLn ""
-
-            setSGR [SetColor Foreground Vivid White]
-            putStrLn ("Bumped version to: v" <> v)
-
+findCabalfile :: IO (Maybe FilePath)
 findCabalfile = do
-    mgitIgnore <- listToMaybe <$> glob ".gitignore"
-    case mgitIgnore of
-        Nothing -> findCabalfile'
-        Just gitIgnore -> do
-            gitIgnoreC <- lines <$> readFile gitIgnore
-            let ignoringCabalFile = isJust $ find ((== ".cabal") . takeExtension) gitIgnoreC
-            if ignoringCabalFile
-                then return Nothing
-                else findCabalfile'
+  mgitIgnore <- listToMaybe <$> glob ".gitignore"
+  case mgitIgnore of
+    Nothing -> findCabalfile'
+    Just gitIgnore -> do
+      gitIgnoreC <- lines <$> readFile gitIgnore
+      let ignoringCabalFile =
+            isJust $ find ((== ".cabal") . takeExtension) gitIgnoreC
+      if ignoringCabalFile
+        then return Nothing
+        else findCabalfile'
   where
     findCabalfile' = listToMaybe <$> glob "./*.cabal"
 
 main :: IO ()
 main = do
-    as <- getArgs
-
-    when (listToMaybe as == Just "help") $ do
-        putStrLn "Usage: stack-bump <patch|minor|major|other <n>>"
-        exitSuccess
-
-    case readBumpType as of
-        Left err -> error err
-        Right bumpType -> run bumpType
+  as <- getArgs
+  when (listToMaybe as == Just "help") $ do
+    putStrLn "Usage: stack-bump <patch|minor|major|other <n>>"
+    exitSuccess
+  case readBumpType as of
+    Left err       -> error err
+    Right bumpType -> run bumpType
--- src/StackBump/Spinner.hs
+++ src/StackBump/Spinner.hs
@@ -2,61 +2,64 @@ module StackBump.Spinner where
 
 import           Control.Concurrent
 import           Control.Concurrent.Async
-import           Control.Exception
 import           Control.Monad
 import           Data.Monoid
 import           System.Console.ANSI
-import           System.IO (hSetBuffering, BufferMode(NoBuffering), stdout,
-                            hFlush, hPutStrLn, stderr)
+import           System.Exit
+import           System.IO                (BufferMode (NoBuffering), hFlush,
+                                           hPutStrLn, hSetBuffering, stderr,
+                                           stdout)
 import           System.IO.Unsafe
 import           System.Process
-import           System.Exit
 
 spinner :: Int -> String
-spinner i = (states !! (i `mod` l)):""
+spinner i = (states !! (i `mod` l)) : ""
   where
     states = "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
     l = length states
 
 counter :: MVar Int
 counter = unsafePerformIO (newMVar 0)
-{-# NOINLINE counter #-}
 
+{-# NOINLINE counter #-}
 displaySpinner :: String -> Async () -> IO ()
 displaySpinner title action = do
-    hSetBuffering stdout NoBuffering
-    setSGR [SetColor Foreground Dull White]
-    putStr (" > ")
-    setSGR [SetColor Foreground Vivid White]
-    putStr (title <> " ")
-    setSGR [SetColor Foreground Vivid Magenta]
-    putStr "⠋"
-    a <- async $ forever $ do
-        currentCount <- modifyMVar counter (\i -> return (i + 1, i))
-        cursorBackward 1
-        setSGR [ SetColor Foreground Vivid Magenta
-               , SetConsoleIntensity BoldIntensity
-               ]
-        putStr (spinner currentCount)
-        hFlush stdout
-        threadDelay (50 * 1000)
-    wait action
-    cancel a
-    clearLine
-    setCursorColumn 0
+  hSetBuffering stdout NoBuffering
+  setSGR [SetColor Foreground Dull White]
+  putStr " > "
+  setSGR [SetColor Foreground Vivid White]
+  putStr (title <> " ")
+  setSGR [SetColor Foreground Vivid Magenta]
+  putStr "⠋"
+  a <-
+    async $
+    forever $ do
+      currentCount <- modifyMVar counter (\i -> return (i + 1, i))
+      cursorBackward 1
+      setSGR
+        [SetColor Foreground Vivid Magenta, SetConsoleIntensity BoldIntensity]
+      putStr (spinner currentCount)
+      hFlush stdout
+      threadDelay (50 * 1000)
+  wait action
+  cancel a
+  clearLine
+  setCursorColumn 0
 
 runProcessWithSpinner :: String -> IO ()
 runProcessWithSpinner title =
-    let (c:args) = words title
-    in runProcessWithSpinnerRaw title c args
+  let (c:args) = words title
+  in runProcessWithSpinnerRaw title c args
 
 runProcessWithSpinnerRaw :: String -> String -> [String] -> IO ()
 runProcessWithSpinnerRaw title c args = do
-    displaySpinner title =<< (async $ do
-        (ec, out, err) <- readProcessWithExitCode c args ""
-        when (ec /= ExitSuccess) $ do
-            setSGR [SetColor Foreground Vivid Red]
-            hPutStrLn stderr out
-            hPutStrLn stderr err
-            setSGR [Reset]
-            error $ title <> " failed with " <> show ec)
+  a <-
+    async $ do
+      (ec, out, err) <- readProcessWithExitCode c args ""
+      when (ec /= ExitSuccess) $ do
+        setSGR [SetColor Foreground Vivid Red]
+        hPutStrLn stderr out
+        hPutStrLn stderr err
+        setSGR [Reset]
+        error $ title <> " failed with " <> show ec
+  displaySpinner title a
